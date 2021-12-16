786a86bce90e068f062f08d8b7070e94518c2011
wujieli0207 <lwj0207@foxmail.com>
2021-12-16 11:44:08 +0800

    build：增加接口过滤器，拦截器，引入 swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,63 @@
 import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import envConfig from '../config/env';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostEntity } from './post/post.entity';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 设置为全局
+      envFilePath: [envConfig.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // 数据库类型
+        entities: [PostEntity], // 数据表实体
+        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT', 3306),
+        username: configService.get('DB_USER', ''),
+        password: configService.get('DB_PASSWORD', ''),
+        database: configService.get('DB_DATABASE', ''),
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表
+      }),
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({
+//     isGlobal: true,  // 设置为全局
+//     envFilePath: [envConfig.path]
+//    }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       inject: [ConfigService],
+//       useFactory: async (configService: ConfigService) => ({
+//         type: 'mysql', // 数据库类型
+//         entities: [],  // 数据表实体
+//         host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+//         port: configService.get<number>('DB_PORT', 3306), // 端口号
+//         username: configService.get('DB_USER', 'root'),   // 用户名
+//         password: configService.get('DB_PASSWORD', 'root'), // 密码
+//         database: configService.get('DB_DATABASE', 'blog'), //数据库名
+//         timezone: '+08:00', //服务器上配置的时区
+//         synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+//       }),
+//     }),
+//     PostsModule,
+//   ],
+//  ...
+// })
+// export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 定义统一接口前缀
+  app.setGlobalPrefix('api');
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 注册全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 注册全局管道
+  app.useGlobalPipes(new ValidationPipe());
+  // 注册 swagger 文档
+  const config = new DocumentBuilder()
+    .setTitle('博客管理后台')
+    .setDescription('博客管理后台接口文档')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(9080);
 }
 bootstrap();
